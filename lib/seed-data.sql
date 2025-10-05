06599e432d46630f0443b740c445b445fe877cd3
AmanAyoub <aman.ayoub16@gmail.com>
2025-10-05 08:15:28 +0430

    seed data for db

---
+++ lib/seed-data.sql
@@ -0,0 +1,7 @@
+INSERT INTO todolists (title)
+  VALUES ('Work Todos'), ('Home Todos'), ('Additional Todos'), ('social todos');
+
+INSERT INTO todos (title, done, todolist_id)
+  VALUES ('Get coffee', true, 1), ('Chat wtih co-workers', true, 1), ('Duck out of meeting', DEFAULT, 1),
+         ('Feed the cats', true, 2), ('Go to bed', true, 2), ('Buy milk', true, 2), ('Study for Launch School', true, 2),
+         ('Go to Libby''s birthday part', DEFAULT, 4);
