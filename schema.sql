e1838c41e4537b7a99d754b160ddf1339e442297
AmanAyoub <aman.ayoub16@gmail.com>
2025-10-05 10:18:17 +0430

    set default value for done

--- schema.sql
+++ schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE todolists (
 CREATE TABLE todos (
   id serial PRIMARY KEY,
   title varchar(100) NOT NULL,
-  done boolean DEFAULT true,
+  done boolean DEFAULT false,
   todolist_id int NOT NULL 
     REFERENCES todolists (id) ON DELETE CASCADE
 );
